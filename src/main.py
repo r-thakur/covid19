8894ef2284247b25f181d7df97d1e54db26e59cb
rohit <rohit.thakur@flybits.com>
2020-09-20 18:33:58 -0400

    Dockerized Integration
    Setup structure and added files required for containerization

--- src/main.py
+++ src/main.py
@@ -163,7 +163,7 @@ if __name__ == "__main__":
 
     if (len(sys.argv) > 1):
         if (sys.argv[1] == "server"):
-            server.app.run()
+            server.app.run(host='0.0.0.0')
         else:
             printAll(int(sys.argv[1]))
     else:
