9eb3c882f1d51b5dd6eb5188fe77a218cbd6d623
Rohit Thakur <53233570+r-thakur@users.noreply.github.com>
2021-05-28 12:20:01 -0400

    updating header and changes to the html

--- src/main.py
+++ src/main.py
@@ -256,13 +256,7 @@ def pullCSV():
   'Upgrade-Insecure-Requests': '1',
   'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-  'Sec-Fetch-Site': 'none',
-  'Sec-Fetch-Mode': 'navigate',
-  'Sec-Fetch-User': '?1',
-  'Sec-Fetch-Dest': 'document',
-  'Accept-Language': 'en-US,en;q=0.9',
-  'If-None-Match': '"1622124368.55-63037"',
-  'If-Modified-Since': 'Thu, 27 May 2021 14:06:08 GMT'
+  'Accept-Language': 'en-US,en;q=0.9'
 }
     requestWHeader = Request(csvURL, headers=hdr)
     csvFile = urlopen(requestWHeader,context = ssl.SSLContext())
